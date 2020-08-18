8a9be6324fdcd0138345a3e196ae02d2cf7b4140
Buyaki <rittahbuyaki@gmail.com>
2020-08-18 15:07:03 +0300

    Add scope to the event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,15 +3,15 @@ class Event < ApplicationRecord
     has_many :event_attendances, foreign_key: :attended_event_id
     has_many :attendees, through: :event_attendances, source: :event_attendee
 
-    def self.upcoming_events
+    scope :upcoming_events, -> {
         Event.all.find_all do |event|
             event.date >= Date.today
         end 
-    end
+    }
 
-    def self.previous_events
+    scope :previous_events, -> {
         Event.all.find_all do |event|
             event.date < Date.today
         end
-    end
+    }
 end
