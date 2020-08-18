56b1b696a4a964d6f4a30e8995abd1d5076a1073
Bluette1 <marylene.sawyer@gmail.com>
2020-08-18 15:05:41 +0200

    Add navigation links and styling to the views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
     def current_user
         @current_user
     end
+
+    def logged_in_user
+        'Logged_in user: '
+      end
+    
+      def current_user_name
+        @current_user.name
+      end
+    
+      def current_user_id
+        @current_user.id
+      end
+    
 end
